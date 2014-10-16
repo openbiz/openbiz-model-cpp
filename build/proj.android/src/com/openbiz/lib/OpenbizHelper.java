353340e983eaa92a6832b0b46af1fb7a2ead7f3a
Jixian Wang <jixian@pickatale.com>
2014-10-17 01:09:02 +0800

    update

--- build/proj.android/src/com/openbiz/lib/OpenbizHelper.java
+++ build/proj.android/src/com/openbiz/lib/OpenbizHelper.java
@@ -7,9 +7,12 @@ public class OpenbizHelper {
 	private static Context sContext = null;
 	private static String sFileDirectory;
 	
-	public static void init(final Context pContext) {
-		OpenbizHelper.sContext = pContext;
-		OpenbizHelper.sFileDirectory = pContext.getFilesDir().getAbsolutePath();
+	public static <T> void init(T pContext) {
+		if(pContext instanceof Context){
+			Context context = (Context )pContext;
+			OpenbizHelper.sContext = context;
+			OpenbizHelper.sFileDirectory = context.getFilesDir().getAbsolutePath();
+		}
 	}
 	
 	public static String getWritablePath() {
