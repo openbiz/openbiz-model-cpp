eaceb962928f1ff9388668851e52b1f17b0003bd
Jixian Wang <jixian@pickatale.com>
2014-10-21 11:58:03 +0800

    update

---
+++ build/proj.android/src/com/openbiz/exception/DataFormatInvalidException.java
@@ -0,0 +1,8 @@
+package com.openbiz.exception;
+
+
+public class DataFormatInvalidException extends Exception {
+
+	private static final long serialVersionUID = 6437155278566428690L;
+
+}
---
+++ build/proj.android/src/com/openbiz/exception/ServerErrorException.java
@@ -0,0 +1,6 @@
+package com.openbiz.exception;
+
+public class ServerErrorException extends Exception {
+	private static final long serialVersionUID = -2715350816116266427L;
+
+}
